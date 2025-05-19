1b09784c9cbf51237499e61d0737d71129eda722
Hudson Reis <hudsonpr09@gmail.com>
2025-05-19 14:01:19 -0300

    iniciando projeto com next.js

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import styles from './aside.module.css';
+import logo from './logo.svg';
+
+const Aside = () => {
+  return (
+    <aside className={styles.aside}>
+      {/* <img src="/logo.svg" alt="Logo da Code Connect"/> */}
+      <Image src={logo} alt='Logo da Code Connect' />
+    </aside>
+  )
+}
+
+export default Aside;
