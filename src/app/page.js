a630c1a0fd836b97df6148ea3d1ad4aed77522d3
Hudson Reis <hudsonpr09@gmail.com>
2025-05-22 15:31:29 -0300

    realizando queries personalizadas com skip e take, fazendo pagination

--- src/app/page.js
+++ src/app/page.js
@@ -23,14 +23,36 @@ import db from "../../prisma/db";
 
 async function getAllPosts(page) {
   try {
-    // Inclui o relacionamento com author
+    const postsPerPage = 4;
+    const skipPosts = (page - 1) * postsPerPage;
+
+    // Pega o total de posts
+    const totalPosts = await db.post.count();
+    // Arredonda pra cima o total de posts divido pela quantidade de posts por pagina
+    const totalPages = Math.ceil(totalPosts / postsPerPage);
+
+    const prev = page > 1 ? page - 1 : null;
+    const next = page < totalPages ? page + 1 : null; 
+
     const data = await db.post.findMany({
+      // Quantos itens pegar
+      take: postsPerPage,
+
+      // Pula os posts
+      skip: skipPosts,
+
+      // Ordem dos posts, em decrescente
+      orderBy: {
+        createdAt: 'desc'
+      },
+
+      // Inclui o relacionamento com author
       include: {
         author: true
       }
     });
 
-    return { data, prev: null, next: null };
+    return { data, prev: prev, next: next };
   } catch(error) {
     logger.error("Falha ao obter posts", { error })
     return { data: [], prev: null, next: null };
@@ -39,9 +61,10 @@ async function getAllPosts(page) {
 
 export default async function Home({ searchParams }) {
   // searchParams são os paramentos de busca na navegação! ?page=1, ?page=2...
-  const currentPage = searchParams?.page || 1;
+  const currentPage = parseInt(searchParams?.page) || 1;
   const { data, prev, next } = await getAllPosts(currentPage);
-
+  console.log(prev);
+  console.log(next);
   return (
       <main className={styles.main}>
         {data.map((post) => 
@@ -52,9 +75,9 @@ export default async function Home({ searchParams }) {
         )}
         {/* <Link> funciona como uma tag <a> super poderosa! */}
         <footer className={styles.footer}>
-          <Link href={`/?page=${prev}`} className={next ? styles.disabled : styles.enable}>Página anterior</Link>
+          <Link href={`/?page=${prev}`} className={prev === null ? styles.disabled : styles.enable}>Página anterior</Link>
           <span>{currentPage}</span>
-          <Link href={`/?page=${next}`} className={prev ? styles.disabled : styles.enable}>Próxima página</Link>
+          <Link href={`/?page=${next}`} className={next === null ? styles.disabled : styles.enable}>Próxima página</Link>
         </footer>
       </main>
   );
